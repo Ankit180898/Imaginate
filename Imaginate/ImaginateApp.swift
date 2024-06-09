6edec41af6b318af855be7c729ca16bb1f234124
Ankit180898 <karan180898@gmail.com>
2024-06-09 20:12:24 +0530

    Initial Commit

---
+++ Imaginate/ImaginateApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImaginateApp.swift
+//  Imaginate
+//
+//  Created by Ankit Kumar on 09/06/24.
+//
+
+import SwiftUI
+
+@main
+struct ImaginateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
